328ab5e4575fe1f6ce08683d8bae13b103ab2e77
Mustofa <mustofa.luthfi@gmail.com>
2019-04-03 19:10:50 +0100

    Aditmadzs

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def handle_message(event):
         )
         line_bot_api.reply_message(event.reply_token, message)
     elif text == 'TakeFoto':
-        url = request.url_root + '/static/logo.png'
+        url = request.url_root + '/coba/image.jpg'
         app.logger.info("url=" + url)
         line_bot_api.reply_message(
             event.reply_token,
