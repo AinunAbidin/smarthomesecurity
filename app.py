e15ee6d2e19480e9de25d97826d26e5b4e27db2e
Mustofa <mustofa.luthfi@gmail.com>
2019-04-03 19:16:02 +0100

    nadyalulussekarang

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def handle_message(event):
         )
         line_bot_api.reply_message(event.reply_token, message)
     elif text == 'TakeFoto':
-        url = request.url_root + '/coba/image.jpg'
+        url = request.url_root + '/home/pi/Desktop/nadyalulussekarang/coba/image.jpg'
         app.logger.info("url=" + url)
         line_bot_api.reply_message(
             event.reply_token,
