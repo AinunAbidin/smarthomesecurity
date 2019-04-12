fb86f67c11aac33e402689604f755e3eeffdc661
Mustofa <mustofa.luthfi@gmail.com>
2019-04-12 16:52:43 +0100

    nadyalulusekarangs

--- app.py
+++ app.py
@@ -108,9 +108,8 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, message)
     elif text == 'TakeFoto':
 	message = ImageSendMessage(
-		original_content_url = 'coba.jpg',
-		priview_content_url = 'coba.jpg'
-	)
+	 original_content_url = 'coba.jpg',
+	 priview_content_url = 'coba.jpg')
         line_bot_api.reply_message(
                 event.reply_token,
                 message)
