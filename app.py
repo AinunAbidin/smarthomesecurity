b66d88fd063a8cdcd7c95f1186725f0d26a1b5b1
Mustofa <mustofa.luthfi@gmail.com>
2019-05-12 21:03:49 +0100

    nadyacantik

--- app.py
+++ app.py
@@ -120,6 +120,7 @@ def handle_message(event):
         )
         line_bot_api.reply_message(event.reply_token, message)
     elif text == 'TakeFoto':
+        print(sender)
         message = ImageSendMessage(
           original_content_url= "https://nadyalulussekarang.herokuapp.com/coba/image.jpg",
           preview_image_url= "https://nadyalulussekarang.herokuapp.com/coba/image.jpg")
