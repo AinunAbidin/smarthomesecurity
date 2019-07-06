bd182ee3730d83e6516192f47d06d0286ed9d0cd
Ainun Abidin <ainun.ilabaca@gmail.com>
2019-07-07 02:11:45 +0700

    haikal

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, abort
-from blob import (
-	write_blob,read_blob
-)
+from subprocess import call
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -12,21 +11,21 @@ from linebot.models import *
 import requests, json
 
 import errno
-import psycopg2
 import os
 import sys, random
 import tempfile
 import time
+import datetime
 
-#import RPi.GPIO as GPIO
-#GPIO.setwarnings(False)
-#GPIO.setmode(GPIO.BOARD)
-#GPIO.setup(11, GPIO.IN)
-#from picamera import PiCamera
-#camera = PiCamera()
-#camera.resolution = (640,480)
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.IN)
+from picamera import PiCamera
+camera = PiCamera()
+camera.resolution = (640,480)
 
-
+x = True
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
     SourceUser, SourceGroup, SourceRoom,
@@ -41,28 +40,71 @@ from linebot.models import (
     TextComponent, SpacerComponent, IconComponent, ButtonComponent,
     SeparatorComponent,
 )
-
 app = Flask(__name__)
 app = Flask(__name__, static_url_path = "/coba", static_folder = "coba")
 # Channel Access Token
-line_bot_api = LineBotApi('h4A/0EsqXaNhrkUTvd5GrtIle2lu65p0j79t6nEpt70ZBKEUfYBCCzsK9dwv+WIx0TpEM8n9YTrOBcP3ifRfWp/AnqEnTCv/6o/yUAoQNXUsuh7qMSjsVbX5j4i3uP7mS7SanV4kCf9XAJu2zDXT4wdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('DLKJqrx5Yc1E56V/+yW8kQ/d2ZuO9ZA4TvbVc7j035VdBz7FuKvt8IH0HVF48tZpa1bx2uXR8vM3/XMfcl4kpjBDy8smRcy6m0uSNwR3xC1DpbFxahcixD2c4JcngMySfKPFhefecAp4/j64AwxZJwdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
-handler = WebhookHandler('aa374d45e8f358df5be55b2c54525695')
+handler = WebhookHandler('f91475fe556eb7fa6c40b48327a9f5e1')
 #===========[ NOTE SAVER ]=======================
 notes = {}
+
 def take_photo():
-        camera.capture('https://nadyalulussekarang.herokuapp.com/coba/image.jpg')
+	data= datetime.datetime.now().strftime("%Y:%m:%D:%H:%M:%S")
+	camera.start_preview()
+	time.sleep(5)
+	camera.capture('coba/image.jpg')
+	camera.stop_preview()
+	image1 ="/home/pi/Downloads/nadyalulussekarang/coba/image.jpg"
+	image2 ="/home/pi/Downloads/nadyalulussekarang/coba/image1.jpg"
+	call(["convert",image1, "-resize", "320x240",image2])
+	time.sleep(2)
+def take_video():
+	data= time.strftime("%d_%b_%Y\%H:%M:%S:%f")
+	camera.start_preview()
+	camera.start_recording('coba/rec.H264')
+	time.sleep(5)
+	camera.stop_recording()
+	camera.stop_preview()
+	video1 ="/home/pi/Downloads/nadyalulussekarang/coba/rec.H264"
+	video2 ="/home/pi/Downloads/nadyalulussekarang/coba/rec.mp4"
+	call(["rm",video2])
+	call(["MP4Box", "-add", video1,video2])
+	time.sleep(2)
+def alat(x):
+	yx=0
+	while x == True:
+		i=GPIO.input(11)
+		if i==0:
+			print(i)
+			time.sleep(2)
+			yx=0
+		elif i==1:
+			print("Motion Detect")
+			yx = yx + 1
+			if yx==3:
+				x = False
+				line_bot_api.broadcast(TextSendMessage(text='Motion Detect'))
+				line_bot_api.broadcast(TextSendMessage(text='Type "Menu" Or "Next" For Ignore '))
+				take_photo()
+				take_video()
+
+			#print(send_message())
+			time.sleep(2)
+
+
+alat(x)
 # Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
+	signature = request.headers['X-Line-Signature']
+	body = request.get_data(as_text=True)
+	app.logger.info("Request body: " + body)
+	try:
+	    handler.handle(body, signature)
+	except InvalidSignatureError:
+	    abort(400)
+	return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -70,12 +112,12 @@ def handle_message(event):
     sender = event.source.user_id #get user_id
     gid = event.source.sender_id #get group_id
 #=====[ TEMPLATE MESSAGE ]=============
-    if text == 'Template':
+    if text == 'Menu':
         buttons_template = TemplateSendMessage(
             alt_text='template',
             template=ButtonsTemplate(
-                title='Choose The Option',
-                text= 'Tap the Button',
+                title='Menu',
+                text= 'Choose One Of The Menu',
                 actions=[
                     MessageTemplateAction(
                         label='Take Video',
@@ -88,167 +130,51 @@ def handle_message(event):
                 ]
             )
         )
+        print(event.reply_token)
         line_bot_api.reply_message(event.reply_token, buttons_template)
-#=====[ CAROUSEL MESSAGE ]==========
-    elif text == '/carousel':
-        message = TemplateSendMessage(
-            alt_text='OTHER MENU',
-            template=CarouselTemplate(
-                columns=[
-                    CarouselColumn(
-                        title='ADD ME',
-                        text='Contact Aditmadzs',
-                        actions=[
-                            URITemplateAction(
-                                label='>TAP HERE<',
-                                uri='https://line.me/ti/p/~adit_cmct'
-                            )
-                        ]
-                    ),
-                    CarouselColumn(
-                        title='Instagram',
-                        text='FIND ME ON INSTAGRAM',
-                        actions=[
-                            URITemplateAction(
-                                label='>TAP HERE!<',
-                                uri='http://line.me/ti/p/~adit_cmct'
-                            )
-                        ]
-                    )
-                ]
-            )
-        )
-        line_bot_api.reply_message(event.reply_token, message)
-    elif text == 'TakeFoto':
-        print(sender)
-        message = ImageSendMessage(
-          original_content_url= "https://nadyalulussekarang.herokuapp.com/coba/image.jpg",
-          preview_image_url= "https://nadyalulussekarang.herokuapp.com/coba/image.jpg")
-        line_bot_api.reply_message(
-                event.reply_token,
-                message)
-    elif text == 'TakeVideo':
-        url = request.url_root + 'image.jpg'
-        app.logger.info("url=" + url)
-        line_bot_api.reply_message(
-            event.reply_token,
-            ImageSendMessage(url, url)
-        )
 
-#=====[ FLEX MESSAGE ]==========
-    elif text == 'flex':
-        bubble = BubbleContainer(
-            direction='ltr',
-            hero=ImageComponent(
-                url='https://lh5.googleusercontent.com/VoOmR6tVRwKEow0HySsJ_UdrQrqrpwUwSzQnGa0yBeqSex-4Osar2w-JohT6yPu4Vl4qchND78aU2c5a5Bhl=w1366-h641-rw',
-                size='full',
-                aspect_ratio='20:13',
-                aspect_mode='cover',
-                action=URIAction(uri='http://line.me/ti/p/~adit_cmct', label='label')
-            ),
-            body=BoxComponent(
-                layout='vertical',
-                contents=[
-                    # title
-                    TextComponent(text='Aditmadzs', weight='bold', size='xl'),
-                    # review
-                    BoxComponent(
-                        layout='baseline',
-                        margin='md',
-                        contents=[
-                            IconComponent(size='sm', url='https://example.com/gold_star.png'),
-                            IconComponent(size='sm', url='https://example.com/grey_star.png'),
-                            IconComponent(size='sm', url='https://example.com/gold_star.png'),
-                            IconComponent(size='sm', url='https://example.com/gold_star.png'),
-                            IconComponent(size='sm', url='https://example.com/grey_star.png'),
-                            TextComponent(text='4.0', size='sm', color='#999999', margin='md',
-                                          flex=0)
-                        ]
-                    ),
-                    # info
-                    BoxComponent(
-                        layout='vertical',
-                        margin='lg',
-                        spacing='sm',
-                        contents=[
-                            BoxComponent(
-                                layout='baseline',
-                                spacing='sm',
-                                contents=[
-                                    TextComponent(
-                                        text='Place',
-                                        color='#aaaaaa',
-                                        size='sm',
-                                        flex=1
-                                    ),
-                                    TextComponent(
-                                        text='Tangerang, Indonesia',
-                                        wrap=True,
-                                        color='#666666',
-                                        size='sm',
-                                        flex=5
-                                    )
-                                ],
-                            ),
-                            BoxComponent(
-                                layout='baseline',
-                                spacing='sm',
-                                contents=[
-                                    TextComponent(
-                                        text='Time',
-                                        color='#aaaaaa',
-                                        size='sm',
-                                        flex=1
-                                    ),
-                                    TextComponent(
-                                        text="10:00 - 23:00",
-                                        wrap=True,
-                                        color='#666666',
-                                        size='sm',
-                                        flex=5,
-                                    ),
-                                ],
-                            ),
-                        ],
-                    )
-                ],
-            ),
-            footer=BoxComponent(
-                layout='vertical',
-                spacing='sm',
-                contents=[
-                    # separator
-                    SeparatorComponent(),
-                    # websiteAction
-                    ButtonComponent(
-                        style='link',
-                        height='sm',
-                        action=URIAction(label='Aditmadzs', uri="https://line.me/ti/p/~adit_cmct")
-                    )
-                ]
-            ),
-        )
-        message = FlexSendMessage(alt_text="hello", contents=bubble)
-        line_bot_api.reply_message(
-            event.reply_token,
-            message
-        )
+    elif text == 'TakeFoto':
+        line_bot_api.push_message(sender, TextSendMessage(text='Image Captured Successfully'))
+        data= datetime.datetime.now().strftime("%Y:%m:%D:%H:%M:%S:%f")
+        line_bot_api.push_message(sender, TextSendMessage(text='at : '+data))
+	#take_photo()
+        time.sleep(1)
+        message = ImageSendMessage(
+          original_content_url='https://nadya.serveo.net/coba/image.jpg',
+          preview_image_url='https://nadya.serveo.net/coba/image1.jpg')
+        line_bot_api.reply_message(event.reply_token,message)
+        print(event.reply_token)
+        data= datetime.datetime.now().strftime("%Y:%m:%D:%H:%M:%S:%f")
+        line_bot_api.push_message(sender, TextSendMessage(text='at : '+data))
+        time.sleep(5)
+        try:
+           line_bot_api.push_message(sender, TextSendMessage(text='Type "Next" For Detecting Motion'))
+        except LineBotApiError as e:
+           abort(400)
+    elif text == 'TakeVideo':
+        line_bot_api.push_message(sender, TextSendMessage(text='Video Recorded Successfully'))
+        data= datetime.datetime.now().strftime("%Y:%m:%D:%H:%M:%S:%f")
+        line_bot_api.push_message(sender, TextSendMessage(text='at : '+data))
+	#take_photo()
+        time.sleep(1)
+        message = VideoSendMessage(
+   	  original_content_url='https://nadya.serveo.net/coba/rec.mp4',
+    	  preview_image_url='https://nadya.serveo.net/coba/image1.jpg')
+        line_bot_api.reply_message(event.reply_token, message)
+        data= datetime.datetime.now().strftime("%Y:%m:%D:%H:%M:%S:%f")
+        line_bot_api.push_message(sender, TextSendMessage(text='at : '+data))
+        time.sleep(5)
+        try:
+           line_bot_api.push_message(sender, TextSendMessage(text='Type "Next" For Detecting Motion'))
+        except LineBotApiError as e:
+           abort(400)
+    elif text == 'Next':
+        x = True
+        alat(x)
 #=======================================================================================================================
 import os
 
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
-   # while True:
-    	#i=GPIO.input(11)
-    	#if i==0:
-        	#print(i)
-        	#time.sleep(0.1)
-    	#elif i==1:
-        	#print("Motion Detect")
-        	#take_photo()
-        	#print(send_message())
-        	#time.sleep(0.1)
-
+    app.run(host='localhost', port=port)
